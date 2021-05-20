cf43ec5a225c082332a49b2291b88e2da700410c
Preston Rozwood <279pdbz199@gmail.com>
2021-05-20 12:23:17 -0700

    Accuracy update

--- cartpole.py
+++ cartpole.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 from continuous_cartpole import CartPoleEnv
 
 env = CartPoleEnv()
-env.reset()
+# env.reset()
 
 #%% Data
 X = np.load('optimal-agent/cartpole-states.npy').T
@@ -84,74 +84,74 @@ def get_action(x):
     return int(np.around(sigmoid(action)))
 
 #%% Error testing
-print("Training error:")
-norms = []
-for state_index in range(X_train.shape[1]):
-    true_action = U_train[:,state_index]
-    predicted_action = np.array([get_action(X_train[:,state_index])])
-    l2_norm = helpers.l2_norm(true_action, predicted_action)
-    norms.append(l2_norm)
-norms = np.array(norms)
-print("Mean difference:", np.mean(norms))
-accuracy = (norms.shape[0]-np.count_nonzero(norms)) / norms.shape[0]
-print("Percent accuracy:", accuracy * 100)
+# print("Training error:")
+# norms = []
+# for state_index in range(X_train.shape[1]):
+#     true_action = U_train[:,state_index]
+#     predicted_action = np.array([get_action(X_train[:,state_index])])
+#     l2_norm = helpers.l2_norm(true_action, predicted_action)
+#     norms.append(l2_norm)
+# norms = np.array(norms)
+# print("Mean difference:", np.mean(norms))
+# accuracy = (norms.shape[0]-np.count_nonzero(norms)) / norms.shape[0]
+# print("Percent accuracy:", accuracy * 100)
 
-print("\nValidation error:")
-norms = []
-for state_index in range(X_validation.shape[1]):
-    true_action = U_validation[:,state_index]
-    predicted_action = np.array([get_action(X_validation[:,state_index])])
-    l2_norm = helpers.l2_norm(true_action, predicted_action)
-    norms.append(l2_norm)
-norms = np.array(norms)
-print("Mean difference:", np.mean(norms))
-accuracy = (norms.shape[0]-np.count_nonzero(norms)) / norms.shape[0]
-print("Percent accuracy:", accuracy * 100)
+# print("\nValidation error:")
+# norms = []
+# for state_index in range(X_validation.shape[1]):
+#     true_action = U_validation[:,state_index]
+#     predicted_action = np.array([get_action(X_validation[:,state_index])])
+#     l2_norm = helpers.l2_norm(true_action, predicted_action)
+#     norms.append(l2_norm)
+# norms = np.array(norms)
+# print("Mean difference:", np.mean(norms))
+# accuracy = (norms.shape[0]-np.count_nonzero(norms)) / norms.shape[0]
+# print("Percent accuracy:", accuracy * 100)
 
-# Both Nystroem and RBF Sampler give the same results:
-# Training error:
-# Mean difference: 0.1479375
-# Percent accuracy: 85.20625
-# Validation error:
-# Mean difference: 0.143
-# Percent accuracy: 85.7
+# # Both Nystroem and RBF Sampler give the same results:
+# # Training error:
+# # Mean difference: 0.1479375
+# # Percent accuracy: 85.20625
+# # Validation error:
+# # Mean difference: 0.143
+# # Percent accuracy: 85.7
 
-#%% Decompose Koopman
-u, s, vh = helpers.SVD(K)
-sv_functions = lambda l, x: np.inner(vh[l], psi(x))
+# #%% Decompose Koopman
+# u, s, vh = helpers.SVD(K)
+# sv_functions = lambda l, x: np.inner(vh[l], psi(x))
 
-#%%
-plt.imshow(env.render(vh[0], 'rgb_array'))
-#%%
-plt.imshow(env.render(vh[1], 'rgb_array'))
-#%%
-plt.imshow(env.render(vh[2], 'rgb_array'))
-#%%
-plt.imshow(env.render(vh[3], 'rgb_array'))
+# #%%
+# plt.imshow(env.render(vh[0], 'rgb_array'))
+# #%%
+# plt.imshow(env.render(vh[1], 'rgb_array'))
+# #%%
+# plt.imshow(env.render(vh[2], 'rgb_array'))
+# #%%
+# plt.imshow(env.render(vh[3], 'rgb_array'))
 
-#%%
-# Xs = np.append(X_train[0,:100].reshape(1,-1), np.array([X_train[1:,0] for i in range(100)]).T, axis=0).T
-Xs = X[:,:100].T
-sv_function_0_outputs = np.array([sv_functions(0, x) for x in Xs])
-sv_function_1_outputs = np.array([sv_functions(1, x) for x in Xs])
-sv_function_2_outputs = np.array([sv_functions(2, x) for x in Xs])
-sv_function_3_outputs = np.array([sv_functions(3, x) for x in Xs])
+# #%%
+# # Xs = np.append(X_train[0,:100].reshape(1,-1), np.array([X_train[1:,0] for i in range(100)]).T, axis=0).T
+# Xs = X[:,:100].T
+# sv_function_0_outputs = np.array([sv_functions(0, x) for x in Xs])
+# sv_function_1_outputs = np.array([sv_functions(1, x) for x in Xs])
+# sv_function_2_outputs = np.array([sv_functions(2, x) for x in Xs])
+# sv_function_3_outputs = np.array([sv_functions(3, x) for x in Xs])
 
-#%%
-plt.plot(sv_function_0_outputs, marker='.', linestyle='')
-plt.show()
-#%%
-plt.plot(sv_function_1_outputs, marker='.', linestyle='')
-plt.show()
-#%%
-plt.plot(sv_function_2_outputs, marker='.', linestyle='')
-plt.show()
-#%%
-plt.plot(sv_function_3_outputs, marker='.', linestyle='')
-plt.show()
+# #%%
+# plt.plot(sv_function_0_outputs, marker='.', linestyle='')
+# plt.show()
+# #%%
+# plt.plot(sv_function_1_outputs, marker='.', linestyle='')
+# plt.show()
+# #%%
+# plt.plot(sv_function_2_outputs, marker='.', linestyle='')
+# plt.show()
+# #%%
+# plt.plot(sv_function_3_outputs, marker='.', linestyle='')
+# plt.show()
 
 #%% Run in environment
-episodes = 100
+episodes = 2
 rewards = []
 for episode in range(episodes):
     # reset environment and variables
@@ -161,6 +161,8 @@ for episode in range(episodes):
     done = False
 
     while done == False:
+        env.render()
+
         action = get_action(current_state)
         current_state, reward, done, _ = env.step(action)
         episode_reward += reward
